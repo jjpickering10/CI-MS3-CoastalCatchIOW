35cbd4af225f8e8780f87a42ab53a901547505bc
jjpickering10 <jjpickering10@live.com>
2021-08-04 14:10:46 +0000

    Add basic login functionality

--- app.py
+++ app.py
@@ -44,6 +44,28 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get('username').lower()})
+        
+        if existing_user:
+            if check_password_hash(
+                existing_user['password'], request.form.get('password')):
+                session['user'] = request.form.get('username').lower()
+                flash("Hello, {}".format(request.form.get('username')))
+            else:
+                flash('Incorrect Username and/or Password')
+                return redirect(url_for('login'))
+        
+        else:
+            flash('Incorrect Username and/or Password')
+            return redirect(url_for('login'))
+            
+    return render_template("login.html")
+
+
 @app.route("/get_reviews")
 def get_reviews():
     reviews = mongo.db.reviews.find()
