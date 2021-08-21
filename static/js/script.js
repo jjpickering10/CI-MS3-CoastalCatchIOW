c622f245b0cd30bbb6c48514055830d9d6dc3f52
jjpickering10 <jjpickering10@live.com>
2021-08-21 15:43:02 +0000

    Add materialize modal for defensive programming on delete button

--- static/js/script.js
+++ static/js/script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var carousel = document.querySelectorAll('.carousel');
     var elemsSelect = document.querySelectorAll('select');
     var elemsCollapse = document.querySelectorAll('.collapsible');
+    var elemsModal = document.querySelectorAll('.modal');
 
     var optionsCarousel = {
       padding: 50,
@@ -16,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var instancesCarousel = M.Carousel.init(carousel, optionsCarousel);
     var instancesSelect = M.FormSelect.init(elemsSelect);
     var instancesCollapse = M.Collapsible.init(elemsCollapse);
+    var instances = M.Modal.init(elemsModal);
   });
 
   // Category selection for ask guru categories
